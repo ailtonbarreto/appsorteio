7f79520d6be48a7007ef50809c528d347f6d4ef7
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2025-04-21 18:59:50 -0300

    Add files via upload

---
+++ public/js/config.js
@@ -0,0 +1,12 @@
+const WS_URL = "ws://192.168.0.101:3000";
+
+const STATUS = {
+  WIN: "youwin",
+  LOSE: "youlose",
+};
+
+const ACTIONS = {
+  ADMIN: "admin",
+  DRAW: "draw",
+  CLIENT_COUNT_UPDATE: "clientCountUpdate",
+};
