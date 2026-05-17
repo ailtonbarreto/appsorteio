944136256c629d43e837dbf25f60bf4ee22f8b22
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2026-05-17 17:27:00 -0300

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -2,6 +2,8 @@ const WS_URL =
   (window.location.protocol === "https:" ? "wss://" : "ws://") +
   window.location.host;
 
+const ws = new WebSocket(WS_URL);
+
 const STATUS = {
   WIN: "youwin",
   LOSE: "youlose",
